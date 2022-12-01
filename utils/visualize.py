1d0b652efd292bbc8239705ea45c376d984ea348
Tracy Wang <tracyykqm@gmail.com>
2022-12-01 14:20:33 -0500

    update visualize util functions and add results

--- utils/visualize.py
+++ utils/visualize.py
@@ -7,39 +7,60 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_distortions(k_upper: int, distortions: list):
+def plot_distortions(
+        k_upper: int,
+        distortions: list,
+        fig_size: tuple,
+        file_name: str=None
+        ):
     """
     visualize distortions in a scatter plot
     args:
         - k_upper: the upper bound of k, inclusive
         - distortions: a list of sse/distortions resulting from all k's
+        - fig_size: the size of the plot
+        - file_name: save plot as file_name 'xxx.png'
     """
     k_lst = range(1, k_upper+1)
     
-    plt.figure(figsize=(6,4))
+    plt.figure(figsize=fig_size)
     plt.plot(k_lst, distortions, color='tomato', marker='x')
     plt.xlabel('k')
     plt.ylabel('Distortion')
     plt.xticks(k_lst)
     plt.title('The Elbow Method showing the optimal k')
-    plt.show()
+    # save plot if a file_name is passed in
+    if file_name:
+        parent_path = 'imgs/'
+        save_path = parent_path + file_name
+        plt.savefig(save_path)
+    else:
+        plt.show()
     return
 
-def plot_label_distr(labels: np.ndarray):
+def plot_label_distr(labels: np.ndarray, fig_size: tuple, file_name: str=None):
     """
     visualize distribution of labels in a bar chart
     args:
         - labels: labels of each data point, resulting from kmeans
+        - fig_size: the size of the plot
+        - file_name: save plot as file_name 'xxx.png'
     """
     # find number of unqiue labels and their counts
     unique_labels, label_count = np.unique(labels, return_counts=True)
-    plt.figure(figsize=(6,4))
+    plt.figure(figsize=fig_size)
     plt.bar(unique_labels, label_count, color='tomato')
     plt.xticks(ticks=unique_labels, labels=unique_labels)
     plt.xlabel('Cluster')
     plt.ylabel('Count')
     plt.title('Distribution of Cluster Counts')
-    plt.show
+    # save plot if a file_name is passed in
+    if file_name:
+        parent_path = 'imgs/'
+        save_path = parent_path + file_name
+        plt.savefig(save_path)
+    else:
+        plt.show()
     return 
     
 def plot_return(
@@ -47,7 +68,9 @@ def plot_return(
         returns: np.ndarray,
         k: int,
         nrows: int,
-        ncols: int
+        ncols: int,
+        fig_size: tuple,
+        file_name: str=None
         ):
     """
     visualize distibution of return in a histogram
@@ -57,11 +80,14 @@ def plot_return(
         - k: number of clusters == len(np.unique(labels))
         - nrows: number of subplots in a row
         - ncols: number of subplots in a col
+        - fig_size: the size of the plot
+        - file_name: save plot as file_name 'xxx.png'
     """
-    fig, axs = plt.subplots(nrows, ncols, figsize=(10,10))
+    fig, axs = plt.subplots(nrows, ncols, sharex=True, sharey=True,
+                            figsize=fig_size)
     for cluster_num in range(k):
-        row = cluster_num // nrows
         col = cluster_num % ncols
+        row = (cluster_num-col)//ncols
         # find indices s.t. label == current cluster_num
         cluster_ind = np.argwhere(labels==cluster_num)
         # add histogram
@@ -69,9 +95,19 @@ def plot_return(
                           color='tomato', edgecolor='black')
         # set title
         axs[row, col].set_title('Cluster ' + str(cluster_num))
-    # set x and y labels
-    for ax in axs.flat:
-        ax.set(xlabel='return', ylabel='density')
-    plt.show()
+        # set y labels
+        if col % ncols == 0:
+            axs[row, col].set_ylabel('density')
+        if (row+1) % nrows == 0:
+            axs[row, col].set_xlabel('return')
+        # show ticklabels
+        axs[row, col].set_visible(True)
+        # save plot if a file_name is passed in
+    if file_name:
+        parent_path = 'imgs/'
+        save_path = parent_path + file_name
+        plt.savefig(save_path)
+    else:
+        plt.show()
     return
     
