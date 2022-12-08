b6237c1f84224f43708ef1e56ddf3d9a7482c649
Tracy Wang <tracyykqm@gmail.com>
2022-12-08 15:15:56 -0500

    add report result util function and new result imgs

--- utils/visualize.py
+++ utils/visualize.py
@@ -6,6 +6,8 @@ Util functions to visualize plots
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from statsmodels.graphics.gofplots import qqplot_2samples
 
 def plot_distortions(
         k_upper: int,
@@ -148,4 +150,75 @@ def plot_return_box(
         plt.savefig(save_path)
     else:
         plt.show()
+    return 
+
+
+def plot_qq(
+        labels_train: np.ndarray,
+        labels_test: np.ndarray,
+        return_train: np.ndarray,
+        return_test: np.ndarray,
+        k: int,
+        nrows: int,
+        ncols: int,
+        fig_size: tuple,
+        file_name: str=None
+        ):
+    """
+    visualize qq plot between train return v test return for each cluster
+    args:
+        - labels_train: cluster labels for train data
+        - labels_test: cluster labels for test data
+        - return_train: annualized next day returns for train data
+        - return_test: annualized next day returns for test data
+        - k: number of clusters == len(np.unique(labels))
+        - nrows: number of subplots in a row
+        - ncols: number of subplots in a col
+        - fig_size: the size of the plot
+        - file_name: save plot as file_name 'xxx.png'
+    
+    """
+    
+    fig, axs = plt.subplots(nrows, ncols, sharex=True, sharey=True,
+                            figsize=fig_size)
+    for cluster_num in range(k):
+        col = cluster_num % ncols
+        row = (cluster_num-col)//ncols
+        # find indices s.t. label == current cluster_num
+        cluster_ind_train = np.argwhere(labels_train==cluster_num)
+        cluster_returns_train = return_train[cluster_ind_train]
+        cluster_returns_test = np.argwhere(labels_test==cluster_num)
+        cluster_ind_test = np.argwhere(labels_test==cluster_num)
+        # convert to probplot instances
+        pp_train = sm.ProbPlot(cluster_returns_train)
+        pp_test = sm.ProbPlot(cluster_returns_test)
+        qqplot_2samples(pp_train, pp_test, xlabel='Train Returns',
+                        ylabel='Test Returns', line='s', ax=axs[row, col])
+        axs[row, col].set_title('Cluster ' + str(cluster_num))
+    if file_name:
+        parent_path = 'imgs/'
+        save_path = parent_path + file_name
+        plt.savefig(save_path)
+    else:
+        plt.show()
+    return
+
+
+def report_return_mean_std(k: int, labels: np.ndarray, returns: np.ndarray):
+    """
+    report mean and standard deviation of each cluster return
+    args:
+        - k: number of clusters == len(np.unique(labels))
+        - labels_test: cluster labels
+        - return_train: annualized next day returns for train data
+    """
+    for cluster_num in range(k):
+        cluster_ind = np.argwhere(labels==cluster_num)
+        return_cluster = np.array(returns)[cluster_ind]
+        mean_str = str(round(return_cluster.mean(), 4))
+        var_str = str(round(return_cluster.std(), 4))
+        result_str = 'cluster' + str(cluster_num) + ' return: ' + \
+        'mean ' + mean_str + ', '\
+        'std ' + var_str
+        print(result_str)
     return 
